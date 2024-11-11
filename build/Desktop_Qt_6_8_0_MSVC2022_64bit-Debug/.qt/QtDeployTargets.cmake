5c2b5cee637b50631c2486e988a1747a41fe4183
cccfirst <chen791015@outlook.com>
2024-11-11 19:26:56 +0800

    init

---
+++ build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Pet_FILE E:/QtProject/Pet/build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/Pet.exe)
+set(__QT_DEPLOY_TARGET_Pet_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Pet_RUNTIME_DLLS D:/QT/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;D:/QT/6.8.0/msvc2022_64/bin/Qt6Guid.dll;D:/QT/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
